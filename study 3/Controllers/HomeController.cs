0e42fdd7297c4189423f66578a567196fb43af29
Viktor <xacker101@yandex.ru>
2024-06-29 17:02:44 +0700

    Добавьте файлы проекта.

---
+++ study 3/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using study_3.Models;
+using System.Diagnostics;
+namespace study_3.Controllers;
+using Microsoft.EntityFrameworkCore;
+    public class HomeController : Controller
+    {
+        ApplicationContext db;
+        public HomeController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id != null)
+        {
+        User? user = await db.Users.FirstOrDefaultAsync(parameter => parameter.Id == id);
+            if (user != null)  return View(user);
+        }
+    return NotFound();
+}
+   [HttpPost]
+   public async Task<IActionResult> Delete(int? id)
+    {
+        if (id != null)
+        {
+            User? user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+        return NotFound();
+    }
+[HttpPost]
+public async Task<IActionResult> Edit(User user)
+{
+    db.Users.Update(user);
+    await db.SaveChangesAsync();
+    return RedirectToAction("Index");
+}
+    public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
---
+++ study 3/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using study_3.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
